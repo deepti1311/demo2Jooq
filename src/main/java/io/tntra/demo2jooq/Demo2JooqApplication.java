2c5175ef04fc539a377654ccf7e3325aa89374db
deepti1311 <deeptijakhotra@gmail.com>
2022-09-15 15:58:24 +0530

    "initial commit: db/migration & Jooq generate done"

---
+++ src/main/java/io/tntra/demo2jooq/Demo2JooqApplication.java
@@ -0,0 +1,13 @@
+package io.tntra.demo2jooq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo2JooqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo2JooqApplication.class, args);
+    }
+
+}
---
+++ src/main/resources/db/migration/V20220901__initialise_database.sql
@@ -0,0 +1,21 @@
+/**
+  1. drop flyway_test schema if created
+  2. create flyway_test schema
+*/
+DROP SCHEMA IF EXISTS flyway_test CASCADE;
+
+CREATE SCHEMA flyway_test;
+
+CREATE SEQUENCE flyway_test.s_student_id START WITH 1;
+
+CREATE TABLE flyway_test.Student
+(
+    id            INT         NOT NULL,
+    first_name    VARCHAR(50),
+    last_name     VARCHAR(50) NOT NULL,
+    age           INT,
+    email       VARCHAR(50),
+    collage_name    VARCHAR(50),
+
+    CONSTRAINT pk_t_student PRIMARY KEY (ID)
+);
